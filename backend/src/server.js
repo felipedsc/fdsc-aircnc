aa110bdf2cdb495b4752a3fda609d442ff36d94f
Felipe Carvalho <felipe_fsc@hotmail.com>
2019-10-03 00:30:09 -0300

    2 - Cadastro via web

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+
 const routes = require('./routes');
 
 const app = express();
@@ -7,7 +10,9 @@ const app = express();
 mongoose.connect('mongodb://omnistack:omnistack@test-shard-00-00-ir0mf.mongodb.net:27017,test-shard-00-01-ir0mf.mongodb.net:27017,test-shard-00-02-ir0mf.mongodb.net:27017/test?ssl=true&replicaSet=test-shard-0&authSource=admin&retryWrites=true&w=majority',
     { useNewUrlParser: true, useUnifiedTopology: true });
 
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
