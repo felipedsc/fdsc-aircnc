7f84a6f1053b04056bfb7e7bf546cb0eabafb29d
Felipe Carvalho <felipe_fsc@hotmail.com>
2019-10-05 22:42:57 -0300

    Aula 5 - Socket.io

--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,11 +1,25 @@
 import React, { useState, useEffect } from 'react';
-import { SafeAreaView, ScrollView, StyleSheet, Text, Image, AsyncStorage } from 'react-native';
+import socketio from 'socket.io-client';
+import { SafeAreaView, ScrollView, StyleSheet, Text, Image, AsyncStorage, Alert } from 'react-native';
 
 import SpotList from '../components/SpotList';
 import logo from '../assets/logo.png';
 
 export default function List() {
     const [techs, setTechs] = useState([]);
+
+    useEffect(() => {
+        AsyncStorage.getItem('user')
+            .then(user_id => {
+                const socket = socketio('http://192.168.221.2:3333', {
+                    query: { user_id }
+                });
+
+                socket.on('booking_response', booking => {
+                    Alert.alert(`Sua reserva em ${booking.spot.company} em ${booking.date} foi ${booking.approved ? 'APROVADA' : 'REJEITADA'}`);
+                });
+            });
+    }, []);
 
     useEffect(() => {
         AsyncStorage.getItem('techs').then(storagedTechs => {
